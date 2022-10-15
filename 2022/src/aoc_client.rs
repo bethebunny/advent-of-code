44e2c6943882d33571833913be5040ffb767a6ba
Stef Nelson-Lindall <bethebunny@gmail.com>
2022-10-15 02:47:19 -0700

    better errors in aoc_client

--- 2022/src/aoc_client.rs
+++ 2022/src/aoc_client.rs
@@ -1,13 +1,30 @@
 // use std::collections::HashMap;
 use std::env;
-use std::io::Write;
 use std::sync::Arc;
 use std::{fs, fs::File};
+use std::{io, io::Write};
 
 use reqwest::{blocking::Client, cookie::Jar, Url};
 
-// TODO: more statics
 static CACHE_DIR: &'static str = ".data";
+
+#[derive(Debug)]
+pub enum Error {
+    Fetch(reqwest::Error),
+    Cache(io::Error),
+}
+
+impl From<io::Error> for Error {
+    fn from(error: io::Error) -> Self {
+        Error::Cache(error)
+    }
+}
+
+impl From<reqwest::Error> for Error {
+    fn from(error: reqwest::Error) -> Self {
+        Error::Fetch(error)
+    }
+}
 
 fn session_id() -> String {
     env::var("AOC_SESSION_ID").expect("Must specify AOC_SESSION_ID in env")
@@ -21,23 +38,23 @@ fn cache_path(year: u16, day: u8) -> String {
     format!("{}/{}.{}.txt", CACHE_DIR, year, day)
 }
 
-fn validate_or_create_cache_directory() {
+fn validate_or_create_cache_directory() -> Result<(), io::Error> {
     let meta = fs::metadata(CACHE_DIR);
     if !meta.is_ok() {
-        fs::create_dir_all(CACHE_DIR).expect("failed to make cache dir");
+        fs::create_dir_all(CACHE_DIR)?;
     }
+    Ok(())
 }
 
-// TODO: cache
-pub fn input(year: u16, day: u8) -> Result<String, reqwest::Error> {
-    validate_or_create_cache_directory();
+pub fn input(year: u16, day: u8) -> Result<String, Error> {
+    validate_or_create_cache_directory()?;
     let path = cache_path(year, day);
     match fs::read_to_string(&path) {
         Ok(data) => Ok(data),
         Err(_) => {
             let data = load_input(year, day)?;
-            let mut file = File::create(path).expect("failed to make data file");
-            file.write_all(data.as_bytes());
+            let mut file = File::create(path)?;
+            file.write_all(data.as_bytes())?;
             Ok(data)
         }
     }
@@ -46,7 +63,7 @@ pub fn input(year: u16, day: u8) -> Result<String, reqwest::Error> {
 pub fn load_input(year: u16, day: u8) -> Result<String, reqwest::Error> {
     println!("Loading data from website!");
     let jar = Arc::new(Jar::default());
-    let url = "https://adventofcode.com".parse::<Url>().unwrap();
+    let url: Url = "https://adventofcode.com".parse().unwrap();
     jar.add_cookie_str(&format!("session={}", session_id()), &url);
     let client = Client::builder().cookie_provider(jar).build()?;
     let request = client.get(input_url(year, day)).build()?;
--- 2022/src/day01.rs
+++ 2022/src/day01.rs
@@ -4,10 +4,10 @@ fn main() {
     let data = aoc_client::input(2022, 1).unwrap();
     // A str.split() is a std::str::Split which doesn't support .as_slice, unlike std::split::Split
     // So we'll collect into a Vec.
-    let lines = data.trim().split("\n").collect::<Vec<_>>();
-    let elf_packs = lines.split(|s| *s == "");
+    let lines: Vec<&str> = data.trim().split("\n").collect();
+    let elf_packs = lines.split(|&s| s == "");
     // Collect so we can sort later
-    let mut elf_calories = elf_packs.map(elf_carrying_calories).collect::<Vec<_>>();
+    let mut elf_calories: Vec<u32> = elf_packs.map(elf_carrying_calories).collect();
 
     println!("{}", elf_calories.iter().max().unwrap());
     elf_calories.sort();
@@ -15,5 +15,5 @@ fn main() {
 }
 
 fn elf_carrying_calories(pack: &[&str]) -> u32 {
-    pack.iter().map(|s| s.parse::<u32>().unwrap()).sum()
+    pack.iter().map(|&s| s.parse::<u32>().unwrap()).sum()
 }
