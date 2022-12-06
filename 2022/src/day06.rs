d8949f74978578417661a6de9b211e89e321823f
Stef Nelson-Lindall <bethebunny@gmail.com>
2022-12-05 21:27:29 -0800

    update day06

--- 2022/src/day06.rs
+++ 2022/src/day06.rs
@@ -6,19 +6,12 @@ fn main() {
     let data = aoc_client::input(2022, 6).unwrap();
     // You LITERALLY CANNOT TREAT A STRING AS A BYTE SLICE.
     let bytes: Vec<u8> = data.bytes().collect();
-    let first_marker = bytes
-        .windows(4)
-        .enumerate()
-        .filter(|(_, v)| HashSet::<u8>::from_iter(v.iter().copied()).len() == 4)
-        .next();
+    let all_unique = |b: &[u8]| HashSet::<u8>::from_iter(b.iter().cloned()).len() == b.len();
+    let first_marker = bytes.windows(4).enumerate().find(|(_, v)| all_unique(v));
     // Output the byte _after_ the marker is complete
     println!("{}", first_marker.unwrap().0 + 4);
 
-    let first_start_of_message_marker = bytes
-        .windows(14)
-        .enumerate()
-        .filter(|(_, v)| HashSet::<u8>::from_iter(v.iter().copied()).len() == 14)
-        .next();
+    let first_start_of_message_marker = bytes.windows(14).enumerate().find(|(_, v)| all_unique(v));
     // Output the byte _after_ the marker is complete
     println!("{}", first_start_of_message_marker.unwrap().0 + 14);
 }
