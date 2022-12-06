ab3278d4c03c62776002505b67c7ba8925c348a3
Stef Nelson-Lindall <bethebunny@gmail.com>
2022-12-05 21:31:27 -0800

    update day06

--- 2022/src/day06.rs
+++ 2022/src/day06.rs
@@ -4,9 +4,9 @@ use std::collections::HashSet;
 
 fn main() {
     let data = aoc_client::input(2022, 6).unwrap();
-    // You LITERALLY CANNOT TREAT A STRING AS A BYTE SLICE.
-    let bytes: Vec<u8> = data.bytes().collect();
+    let bytes = data.as_bytes();
     let all_unique = |b: &[u8]| HashSet::<u8>::from_iter(b.iter().cloned()).len() == b.len();
+
     let first_marker = bytes.windows(4).enumerate().find(|(_, v)| all_unique(v));
     // Output the byte _after_ the marker is complete
     println!("{}", first_marker.unwrap().0 + 4);
