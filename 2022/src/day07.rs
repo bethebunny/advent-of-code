75f73f64de1302348b9cdc1aa8c0688369c3f9d9
Stef Nelson-Lindall <bethebunny@gmail.com>
2022-12-07 07:30:55 -0800

    pull stuff out into functions

--- 2022/src/day07.rs
+++ 2022/src/day07.rs
@@ -16,7 +16,33 @@ enum File {
     },
 }
 
+#[derive(Debug)]
+enum Error {
+    FileKindError,
+    NoSuchFile,
+    ParseError,
+}
+
 impl File {
+    fn new_root() -> File {
+        let mut root = File::Directory {
+            files: HashMap::new(),
+            parent: NonNull::dangling(),
+        };
+        let root_ptr = NonNull::from(&mut root);
+        if let File::Directory { ref mut parent, .. } = root {
+            *parent = root_ptr
+        }
+        root
+    }
+
+    fn new_dir(parent: &File) -> File {
+        File::Directory {
+            files: HashMap::new(),
+            parent: NonNull::from(parent),
+        }
+    }
+
     fn size(&self) -> usize {
         match self {
             File::Binary { bytes } => *bytes,
@@ -32,68 +58,64 @@ impl File {
             }
         }
     }
+
+    fn files_mut(&mut self) -> Result<&mut HashMap<String, File>, Error> {
+        match self {
+            File::Directory { files, .. } => Ok(files),
+            _ => Err(Error::FileKindError),
+        }
+    }
+
+    #[allow(dead_code)]
+    fn files(&self) -> Result<&HashMap<String, File>, Error> {
+        match self {
+            File::Directory { files, .. } => Ok(files),
+            _ => Err(Error::FileKindError),
+        }
+    }
 }
 
-fn main() {
-    let data = aoc_client::input(2022, 7).unwrap();
-
-    let mut root = unsafe {
-        let mut dummy = File::Binary { bytes: 0 };
-        let mut _root = File::Directory {
-            files: HashMap::new(),
-            parent: NonNull::new_unchecked(&mut dummy as *mut File),
-        };
-        let root_ptr: *mut File = &mut _root;
-        if let File::Directory { ref mut parent, .. } = _root {
-            *parent = NonNull::new_unchecked(root_ptr);
-        }
-        _root
-    };
+fn parse_filesystem_from_logs(logs: &str) -> Result<File, Error> {
+    let mut root = File::new_root();
     let mut cwd = &mut root;
 
-    for line in data.lines() {
+    for line in logs.lines() {
         if line == "$ cd /" {
             cwd = &mut root;
         } else if line == "$ cd .." {
             if let File::Directory { parent, .. } = cwd {
+                // Yup this is unsafe :P
+                // I think the fix here is to make our file pointers Rc<RefCell<File>>
+                // and then have parent be a Weak<RefCell<File>> so we can enforce
+                // run-time borrow semantics safely.
                 cwd = unsafe { parent.as_mut() };
             }
         } else if line.starts_with("$ cd ") {
-            let (_, subdir) = line.rsplit_once(" ").unwrap();
-            if let File::Directory { files, .. } = cwd {
-                cwd = match files.get_mut(subdir).unwrap() {
-                    File::Binary { .. } => panic!("Can't cd to binary"),
-                    dir => dir,
-                };
-            }
+            let (_, subdir) = line.rsplit_once(" ").ok_or(Error::ParseError)?;
+            cwd = match cwd.files_mut()?.get_mut(subdir).ok_or(Error::NoSuchFile)? {
+                File::Binary { .. } => return Err(Error::FileKindError),
+                dir => dir,
+            };
         } else if line.starts_with("$ ls") {
             // nothing
         } else if line.starts_with("dir ") {
-            let (_, subdir) = line.rsplit_once(" ").unwrap();
-            let cwd_pointer: *mut File = cwd;
-            if let File::Directory { ref mut files, .. } = cwd {
-                files.insert(
-                    subdir.to_string(),
-                    File::Directory {
-                        files: HashMap::new(),
-                        parent: NonNull::new(cwd_pointer).unwrap(),
-                    },
-                );
-            }
+            let (_, subdir) = line.rsplit_once(" ").ok_or(Error::ParseError)?;
+            let new_dir = File::new_dir(cwd);
+            cwd.files_mut()?.insert(subdir.to_string(), new_dir);
         } else {
-            let (size, name) = line
-                .split_once(" ")
-                .expect(format!("Invalid ls size string: '{}'", line).as_str());
-            if let File::Directory { ref mut files, .. } = cwd {
-                files.insert(
-                    name.to_string(),
-                    File::Binary {
-                        bytes: size.parse::<usize>().unwrap(),
-                    },
-                );
-            }
+            let (size, name) = line.split_once(" ").ok_or(Error::ParseError)?;
+            let bytes = size.parse::<usize>().map_err(|_| Error::ParseError)?;
+            cwd.files_mut()?
+                .insert(name.to_string(), File::Binary { bytes });
         }
     }
+
+    Ok(root)
+}
+
+fn main() {
+    let data = aoc_client::input(2022, 7).unwrap();
+    let root = parse_filesystem_from_logs(data.as_str()).unwrap();
 
     let small_dirs = root.walk_dirs().filter(|d| d.size() <= 100000);
     println!("{}", small_dirs.map(|d| d.size()).sum::<usize>());
